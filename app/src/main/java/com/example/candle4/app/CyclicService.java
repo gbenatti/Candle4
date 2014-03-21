2b72c7d7fb52d9a01136e60180bbb81eecbafb84
Georges Benatti Jr <gbenatti@gmail.com>
2014-03-21 20:17:50 -0300

    base glass sample with service and menu

---
+++ app/src/main/java/com/example/candle4/app/CyclicService.java
@@ -0,0 +1,55 @@
+package com.example.candle4.app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by GB on 1/23/14.
+ */
+abstract class CyclicService extends Service{
+
+    private final long DELAY_MILLIS = 1000;
+    private final Handler mHandler = new Handler();
+
+    private boolean mStarted;
+    private long startTime;
+
+    abstract void onUpdate();
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startUpdateCycle();
+
+        return START_STICKY;
+    }
+
+    private void startUpdateCycle() {
+        if (!mStarted) {
+            mStarted = true;
+            startTime = SystemClock.elapsedRealtime();
+            mHandler.postDelayed(mUpdateNewsRunnable, DELAY_MILLIS);
+        }
+    }
+
+    private final Runnable mUpdateNewsRunnable = new Runnable() {
+        @Override
+        public void run() {
+        	if (mStarted) {
+                if (shouldUpdate()) {
+                    startTime = SystemClock.elapsedRealtime();
+
+                    onUpdate();
+                }
+                mHandler.postDelayed(mUpdateNewsRunnable, DELAY_MILLIS);
+        	}
+        }
+
+    };
+
+    private boolean shouldUpdate() {
+        return SystemClock.elapsedRealtime() - startTime > TimeUnit.SECONDS.toMillis(60);
+    }
+}
---
+++ app/src/main/java/com/example/candle4/app/FactsService.java
@@ -0,0 +1,76 @@
+package com.example.candle4.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Binder;
+import android.os.IBinder;
+
+import com.google.android.glass.media.Sounds;
+
+public class FactsService extends CyclicService {
+
+    private FactsManager factsHolder;
+    private FactsCardManager cardManager;
+
+    /**
+     * A binder that gives other components access to the speech capabilities provided by the
+     * service.
+     */
+    public class FactsBinder extends Binder {
+        /**
+         * Read the current heading aloud using the text-to-speech engine.
+         */
+        public void readFactAloud() {
+            factsHolder.sayText();
+        }
+    }
+
+    private final FactsBinder binder = new FactsBinder();
+
+    @Override
+	public void onCreate() {
+		super.onCreate();
+        cardManager = new FactsCardManager(this);
+        factsHolder = new FactsManager(this);
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return binder;
+	}
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        cardManager.createMainCard();
+        cardManager.setText(factsHolder.getText());
+
+        return START_STICKY;
+    }
+
+    @Override
+    void onUpdate() {
+        changeText();
+        playSound();
+    }
+
+    @Override
+    public void onDestroy() {
+        cardManager.destroyMainCard();
+        cardManager = null;
+        factsHolder = null;
+
+        super.onDestroy();
+    }
+
+    private void changeText() {
+        factsHolder.selectNext();
+        cardManager.setText(factsHolder.getText());
+    }
+
+    private void playSound() {
+        AudioManager audio = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        audio.playSoundEffect(Sounds.TAP);
+    }
+}
